b86019e12ea651848971a3c90bb9f492995b9388
PXF <tu619458244@qq.com>
2017-09-06 22:16:07 +0800

    modified:   10.5.sh 	11.sh 	12.sh 	13.sh 	14.sh 	15.sh 	16.sh 	17.sh 	18.sh 	19.sh 	20.sh 	21.sh 	22.sh 	23.sh

--- 10.5.sh
+++ 10.5.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 sortArray=()
 i=0
+tmp=0
 if [ $# -lt 2 ];then
-	echo "输入数字太小"
+	echo "输入数字太少"
 	exit 1
 fi
-prevNum=$1
+#将输入赋值到新的数组中
 for num in $*
 do
 	sortArray[$i]=$num
-	let i++	
+	let i++
 done
+
+#开始排序
+for ((j = 0; j < $i; j++))
+do
+	for ((k = $[$j+1]; k < $i; k++))
+	do
+		if [ ${sortArray[$j]} -gt ${sortArray[$k]} ];then
+			tmp=${sortArray[$k]}
+			sortArray[$k]=${sortArray[$j]}
+			sortArray[$j]=$tmp
+		fi
+	done
+done
+
 #打印排序过后的数组
 echo ${sortArray[@]}
